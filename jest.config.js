c3d5a13d6f18062240f9b54ff6c661afcd01e1a1
Marcos Dias da Conceição <ti.marcosdias@gmail.com>
2022-01-23 18:27:15 -0300

    fix jest config

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,9 @@ module.exports = {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}'],
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+    '!src/**/*.stories.{js,jsx,ts,tsx}'
+  ],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts']
 }
